d2dff498f5f0079a039700f57a580feb8a98df9b
Michael Mell <mike.mell@nthwave.net>
2013-05-03 20:09:49 -0700

    match version to current edge rails

--- lib/active_resource/version.rb
+++ lib/active_resource/version.rb
@@ -1,9 +1,9 @@
 module ActiveResource
   module VERSION #:nodoc:
     MAJOR = 4
-    MINOR = 0
+    MINOR = 1
     TINY  = 0
-    PRE   = "beta1"
+    PRE   = "beta"
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
